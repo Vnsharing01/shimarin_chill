4a12d5a51b6486ef089837a0e0fdff071f005e20
thicmn <thichu08@gmail.com>
2025-06-16 22:22:11 +0700

    add audio player to music play page

--- lib/features/music_play/music_play_page.dart
+++ lib/features/music_play/music_play_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:shimarin_chill/data/models/sound_model.dart';
 import 'package:shimarin_chill/features/music_play/bloc/music_play_bloc.dart';
 import 'package:shimarin_chill/utils/app_text_style.dart';
@@ -30,10 +31,12 @@ class MusicPlayPage extends StatefulWidget {
 }
 
 class _MusicPlayPageState extends State<MusicPlayPage> {
+  late final AudioPlayer _player;
   @override
   void initState() {
     super.initState();
 
+    _player = AudioPlayer();
     context.read<MusicPlayBloc>().add(
           InitData(listMusic: widget.arguments?.sounds ?? []),
         );
@@ -42,6 +45,34 @@ class _MusicPlayPageState extends State<MusicPlayPage> {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeRight,
     ]);
+
+    initAudio();
+  }
+
+  Future<void> initAudio() async {
+    // final state = context.watch<MusicPlayBloc>();
+    final playlist = ConcatenatingAudioSource(
+        shuffleOrder: DefaultShuffleOrder(),
+        useLazyPreparation: true,
+        children: (widget.arguments?.sounds ?? []).map(
+          (path) {
+            return AudioSource.asset(path.filePath!);
+          },
+        ).toList());
+
+    // load playlist
+    await _player.setAudioSource(
+      playlist,
+      initialIndex: 0,
+      preload: true,
+    );
+
+    // trộn playlist
+    _player.setShuffleModeEnabled(true);
+    _player.shuffle();
+
+    // chạy playlist
+    _player.play();
   }
 
   @override
@@ -50,6 +81,7 @@ class _MusicPlayPageState extends State<MusicPlayPage> {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
+    _player.dispose();
     super.dispose();
   }
 
@@ -84,17 +116,23 @@ class _MusicPlayPageState extends State<MusicPlayPage> {
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 200),
-                  child: LinearProgressIndicator(
-                    minHeight: 12,
-                    borderRadius: BorderRadius.circular(8),
-                    value: 50.0.clamp(0.0, 0.1),
-                    backgroundColor: Colors.grey.withOpacity(0.3),
-                    valueColor:
-                        const AlwaysStoppedAnimation<Color>(Colors.white),
-                  ),
-                ),
+                StreamBuilder<PlayerState>(
+                    stream: _player.playerStateStream,
+                    builder: (context, snapshot) {
+                      final playState = snapshot.data;
+                      final playing = playState?.playing ?? false;
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 200),
+                        child: LinearProgressIndicator(
+                          minHeight: 12,
+                          borderRadius: BorderRadius.circular(8),
+                          value: 50.0.clamp(0.0, 0.1),
+                          backgroundColor: Colors.grey.withOpacity(0.3),
+                          valueColor:
+                              const AlwaysStoppedAnimation<Color>(Colors.white),
+                        ),
+                      );
+                    }),
               ],
             ),
           ),
