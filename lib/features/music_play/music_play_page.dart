ffabedd112b219c180a5d01a6c9598a569207c8d
thicmn <thichu08@gmail.com>
2025-06-26 20:31:26 +0700

    fix bug start time duration

--- lib/features/music_play/music_play_page.dart
+++ lib/features/music_play/music_play_page.dart
@@ -88,7 +88,7 @@ class _MusicPlayPageState extends State<MusicPlayPage>
       listenWhen: (previous, current) =>
           previous.durationSelected != current.durationSelected,
       listener: (context, state) {
-        if (duration > Duration.zero) {
+        if (state.durationSelected > Duration.zero) {
           _startDuration(state.durationSelected);
         } else {
           _animationController.stop();
