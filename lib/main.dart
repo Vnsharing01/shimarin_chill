301d1f42eb76e7fd3ff54cd8e74240bd49702a7f
thicmn <thichu08@gmail.com>
2025-07-29 20:15:26 +0700

    remove firebase; add logo icon & update app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,17 +11,12 @@ import 'package:shimarin_chill/data/models/album_model.dart';
 import 'package:shimarin_chill/data/models/sound_model.dart';
 import 'package:shimarin_chill/features/home/bloc/home_bloc.dart';
 import 'package:shimarin_chill/features/setting/bloc/setting_bloc.dart';
-import 'package:shimarin_chill/firebase_options.dart';
 import 'package:shimarin_chill/routes/routes.dart';
 import 'package:shimarin_chill/utils/constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   unawaited(MobileAds.instance.initialize());
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   final prefs = await SharedPreferences.getInstance();
 
